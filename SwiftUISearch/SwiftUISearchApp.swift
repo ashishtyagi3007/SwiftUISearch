b6711c867a169dd508b48576251215444a251dc2
Ashish Tyagi <ashisht@dewsolutions.in>
2020-10-12 20:08:28 +0530

    Initial Commit

---
+++ SwiftUISearch/SwiftUISearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISearchApp.swift
+//  SwiftUISearch
+//
+//  Created by Ashish Tyagi on 12/10/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
